f2b928a5cefe6539b5123ccbff404ed607c8c2c9
Anmol Bhardwaj <anmolbhardwajinv224@gmail.com>
2023-10-19 14:39:01 +0530

    Added sound to the UI Button

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,7 +1,10 @@
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    var player: AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -9,8 +12,126 @@ class ViewController: UIViewController {
 
     
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("I got Pressed")
+        playSound1()
     }
     
+    func playSound1() {
+        guard let path = Bundle.main.path(forResource: "C", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    @IBAction func keyPressed2(_ sender: UIButton) {
+        playSound2()
+    }
+    func playSound2() {
+        guard let path = Bundle.main.path(forResource: "D", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    @IBAction func keyPressed3(_ sender: UIButton) {
+        playSound3()
+    }
+    func playSound3() {
+        guard let path = Bundle.main.path(forResource: "E", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    @IBAction func keyPressed4(_ sender: Any) {
+        playSound4()
+    }
+    func playSound4() {
+        guard let path = Bundle.main.path(forResource: "F", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    
+    @IBAction func keyPressed5(_ sender: UIButton) {
+        playSound5()
+    }
+    
+    func playSound5() {
+        guard let path = Bundle.main.path(forResource: "G", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    @IBAction func keyPressed6(_ sender: UIButton) {
+        playSound6()
+    }
+    func playSound6() {
+        guard let path = Bundle.main.path(forResource: "A", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    @IBAction func keyPressed7(_ sender: UIButton) {
+        playSound7()
+    }
+    func playSound7() {
+        guard let path = Bundle.main.path(forResource: "B", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
