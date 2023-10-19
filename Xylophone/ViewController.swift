ca4a88d749c5880153925b02e600cdebd265cb78
Anmol Bhardwaj <anmolbhardwajinv224@gmail.com>
2023-10-19 14:50:17 +0530

    Some changes

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -5,15 +5,21 @@ class ViewController: UIViewController {
 
     
     var player: AVAudioPlayer?
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
     }
-
-
+    
     
     @IBAction func keyPressed(_ sender: UIButton) {
+        
         playSound1()
+        
     }
+    
     
     func playSound1() {
         guard let path = Bundle.main.path(forResource: "C", ofType:"wav") else {
@@ -31,8 +37,12 @@ class ViewController: UIViewController {
     
     
     @IBAction func keyPressed2(_ sender: UIButton) {
+        
         playSound2()
+        
     }
+    
+    
     func playSound2() {
         guard let path = Bundle.main.path(forResource: "D", ofType:"wav") else {
             return }
@@ -49,8 +59,12 @@ class ViewController: UIViewController {
     
     
     @IBAction func keyPressed3(_ sender: UIButton) {
+        
         playSound3()
+        
     }
+    
+    
     func playSound3() {
         guard let path = Bundle.main.path(forResource: "E", ofType:"wav") else {
             return }
@@ -65,9 +79,14 @@ class ViewController: UIViewController {
         }
     }
     
+    
     @IBAction func keyPressed4(_ sender: Any) {
+        
         playSound4()
+        
     }
+    
+    
     func playSound4() {
         guard let path = Bundle.main.path(forResource: "F", ofType:"wav") else {
             return }
@@ -83,10 +102,12 @@ class ViewController: UIViewController {
     }
     
     
-    
     @IBAction func keyPressed5(_ sender: UIButton) {
+        
         playSound5()
+        
     }
+    
     
     func playSound5() {
         guard let path = Bundle.main.path(forResource: "G", ofType:"wav") else {
@@ -101,9 +122,15 @@ class ViewController: UIViewController {
             print(error.localizedDescription)
         }
     }
+    
+    
     @IBAction func keyPressed6(_ sender: UIButton) {
+        
         playSound6()
+        
     }
+    
+    
     func playSound6() {
         guard let path = Bundle.main.path(forResource: "A", ofType:"wav") else {
             return }
@@ -117,9 +144,15 @@ class ViewController: UIViewController {
             print(error.localizedDescription)
         }
     }
+    
+    
     @IBAction func keyPressed7(_ sender: UIButton) {
+        
         playSound7()
+        
     }
+    
+    
     func playSound7() {
         guard let path = Bundle.main.path(forResource: "B", ofType:"wav") else {
             return }
@@ -133,5 +166,7 @@ class ViewController: UIViewController {
             print(error.localizedDescription)
         }
     }
+    
+    
 }
 
